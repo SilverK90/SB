5e57fe53ab29790f5246c63fcfe28847646dc902
Digvijay1838 <56652519+Digvijay1838@users.noreply.github.com>
2020-09-19 21:58:59 +0530

    update folder structure

--- search.js
+++
@@ -1,5 +0,0 @@
-function Get() {
-    var name = document.getElementById('name').value;
-    var age = (document.getElementById('age').value).toInt();
-    document.getElementById('result').innerHTML = typeof age;
-}
